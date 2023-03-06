527505487a232507464f1639df1dc93438b79d75
Saiful Islam <121204378+saislamb97@users.noreply.github.com>
2023-03-06 08:51:33 +0000

    api routes updated

--- server-reactjs/index.js
+++ server-reactjs/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
+const router = require('./routes/routers')
+const cors = require('cors')
 
-app.get('/', (req, res) => {res.send("Welcome to crud server")})
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+app.use(cors({
+    origin:""
+}))
+
+
+app.use('/', router)
+
+
 
 
 app.listen(4000, () => { console.log("Server is running.") })
