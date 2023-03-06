09746d6b232af9816a46b5b2774b3b2ef79f69bc
Saiful Islam <121204378+saislamb97@users.noreply.github.com>
2023-03-06 08:16:30 +0000

    data inserting form created

--- client-reactjs/src/App.js
+++ client-reactjs/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from 'react';
+import './index.css'
+import Insertdata from './components/Insertform'
+import { useState } from 'react';
+
 
 function App() {
+  const [popup, setPopup] = useState(false)
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+    <div className="fixed top-0 left-0 right-0 w-full p-4  md:inset-0 h-[calc(100%-1rem)] md:h-full bg-amber-300 flex items-center justify-center">
+      <button onClick={()=> setPopup(true)} className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+        Insert Button
+      </button>
+      <Insertdata trigger={popup} setTrigger = {setPopup} />
     </div>
   );
 }
---
+++ client-reactjs/src/components/Insertform.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react'
+import Axios from 'axios'
+import '../index.css'
+
+function Insertdata(props) {
+    const url = ""
+    const [data, setData] = useState({
+        firstname:"",
+        lastname:"",
+        email:"",
+        password:"",
+        city:"",
+        state:"",
+        zip:""
+    })
+    
+    function submit(e){
+        e.preventDefault();
+        Axios.post(url, {
+            firstname:data.firstname,
+            lastname:data.lastname,
+            email:data.email,
+            password:data.password,
+            city:data.city,
+            state:data.state,
+            zip:data.zip
+        }).then(res => console.log(res.data))
+    }
+
+    function handle(e){
+        const newData = {...data}
+        newData[e.target.id] = e.target.value
+        setData(newData)
+        console.log(newData)
+    } 
+
+  return (props.trigger)?(
+
+        <div className="fixed top-0 left-0 right-0 w-full p-4  md:inset-0 h-[calc(100%-1rem)] md:h-full bg-amber-300 flex items-center justify-center">
+            <div className="relative w-full h-full max-w-md md:h-auto">
+
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <button onClick={()=>props.setTrigger(false)} className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
+                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                        <span className="sr-only">Close Form</span>
+                    </button>
+                    <div className="px-6 py-6 lg:px-8 grid h-screen place-items-center">
+                        <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Register Account</h3>
+                        
+                        <form onSubmit={(e)=> submit(e)} className="w-full max-w-lg">
+                            <div className="flex flex-wrap -mx-3 mb-6">
+                                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    First Name
+                                </label>
+                                <input onChange={(e) => handle(e)} value={data.firstname} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="firstname" type="text" placeholder="Jane"></input>
+                                </div>
+                                <div className="w-full md:w-1/2 px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    Last Name
+                                </label>
+                                <input onChange={(e) => handle(e)} value={data.lastname} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="lastname" type="text" placeholder="Doe"></input>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap -mx-3 mb-6">
+                                <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    Email
+                                </label>
+                                <input onChange={(e) => handle(e)} value={data.email} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="email@example.com"></input>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap -mx-3 mb-6">
+                                <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    Password
+                                </label>
+                                <input onChange={(e) => handle(e)} value={data.password} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="password" type="password" placeholder="******************"></input>
+                                <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap -mx-3 mb-2">
+                                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    City
+                                </label>
+                                <input onChange={(e) => handle(e)} value={data.city} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="city" type="text" placeholder="Albuquerque"></input>
+                                </div>
+                                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    State
+                                </label>
+                                <div className="relative">
+                                    <select onChange={(e) => handle(e)} value={data.state} className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="state">
+                                    <option>New Mexico</option>
+                                    <option>Missouri</option>
+                                    <option>Texas</option>
+                                    </select>
+                                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                                    </div>
+                                </div>
+                                </div>
+                                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                    Zip
+                                </label>
+                                <input onChange={(e) => handle(e)} value={data.zip} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="zip" type="text" placeholder="90210"></input>
+                                </div>
+                                <div className="w-full px-3 flex flex-col justify-center items-center mt-5">
+                                <button  className="bg-blue-500 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                Sign Up
+                                </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+  ):""
+}
+
+export default Insertdata
